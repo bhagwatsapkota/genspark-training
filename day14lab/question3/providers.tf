2ce9b8074796a4bb5d015ef8723b8deda4bca0ca
Bhagwat Sapkota <54609817+bhagwatsapkota@users.noreply.github.com>
2022-07-19 00:40:53 -0400

    Update providers.tf

--- day14lab/question3/providers.tf
+++ day14lab/question3/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-2"
-    access_key = "AKIAWWBQEDCAGOZNFVO4"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
